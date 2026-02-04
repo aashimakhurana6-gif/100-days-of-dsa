d25e41314ecd6b8605bff6ea44a5862fd0c4b607
Aashima Khurana <aashimakhurana6@gmail.com>
2026-02-04 19:24:55 +0530

    Add files via upload

---
+++ Day4.c
@@ -0,0 +1,33 @@
+//Given an array of n integers, reverse the array in-place using two-pointer approach.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+
+    
+    scanf("%d", &n);
+
+    
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
