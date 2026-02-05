bba86caea25b207858c42a0c45703721f6df7902
Aashima Khurana <aashimakhurana6@gmail.com>
2026-02-05 20:48:48 +0530

    Add files via upload

---
+++ Day5.c
@@ -0,0 +1,49 @@
+//system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
+#include <stdio.h>
+
+int main() {
+    int p, q;
+    int i = 0, j = 0, k = 0;
+
+  
+    scanf("%d", &p);
+    int a[p];
+    for (i = 0; i < p; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    
+    scanf("%d", &q);
+    int b[q];
+    for (i = 0; i < q; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    int merged[p + q];
+    i = 0;
+    j = 0;
+
+    while (i < p && j < q) {
+        if (a[i] <= b[j]) {
+            merged[k++] = a[i++];
+        } else {
+            merged[k++] = b[j++];
+        }
+    }
+
+    while (i < p) {
+        merged[k++] = a[i++];
+    }
+
+    while (j < q) {
+        merged[k++] = b[j++];
+    }
+
+   
+    for (i = 0; i < p + q; i++) {
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
+
