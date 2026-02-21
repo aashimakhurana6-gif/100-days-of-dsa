71b4b221e6691327f21fd0c587c2812213597570
Aashima Khurana <aashimakhurana6@gmail.com>
2026-02-21 18:53:37 +0530

    Add files via upload

---
+++ Day21.c
@@ -0,0 +1,51 @@
+//Create and Traverse Singly Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    int n, i, value;
+
+    struct node *head = NULL;
+    struct node *temp = NULL;
+    struct node *newnode = NULL;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+
+        newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = value;
+        newnode->next = NULL;
+
+       
+        if(head == NULL)
+        {
+            head = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
