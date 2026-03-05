933d32f4116a84243058934470d5b649b41e424a
Aashima Khurana <aashimakhurana6@gmail.com>
2026-03-06 00:08:10 +0530

    Add files via upload

---
+++ Day32.c
@@ -0,0 +1,53 @@
+//Implement push and pop operations on a stack and verify stack operations.
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    top++;
+    stack[top] = value;
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    top--;
+}
+
+// Display stack from top to bottom
+void display() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+}
+
+int main() {
+    int n, m, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    scanf("%d", &m);
+
+    for (int i = 0; i < m; i++) {
+        pop();
+    }
+
+    display();
+
+    return 0;
+}
