811ba76c54a1a8a11b32b14a582a02b778dd7340
Aashima Khurana <aashimakhurana6@gmail.com>
2026-04-04 23:41:23 +0530

    Add files via upload

---
+++ day61.c
@@ -0,0 +1,43 @@
+//Build a graph with n vertices and m edges using adjacency matrix representation. The graph may be directed or undirected. 
+#include <stdio.h>
+
+int main() {
+    int n, m;
+
+    // Input vertices and edges
+    scanf("%d %d", &n, &m);
+
+    int adj[n][n];
+
+    // Initialize matrix with 0
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    int u, v;
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        // Undirected graph
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+
+        // 👉 For directed graph, use only:
+        // adj[u][v] = 1;
+    }
+
+    // Print adjacency matrix
+    printf("Adjacency Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
