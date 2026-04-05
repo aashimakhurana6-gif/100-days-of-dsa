c7aebf4c8cb8737e2c4508ca22c20f07d28158f1
Aashima Khurana <aashimakhurana6@gmail.com>
2026-04-05 22:53:27 +0530

    Add files via upload

---
+++ day64.c
@@ -0,0 +1,75 @@
+//Perform BFS from a given source using queue.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Queue structure
+int queue[MAX];
+int front = -1, rear = -1;
+
+// Enqueue
+void enqueue(int x) {
+    if (rear == MAX - 1)
+        return;
+    if (front == -1)
+        front = 0;
+    queue[++rear] = x;
+}
+
+// Dequeue
+int dequeue() {
+    if (front == -1)
+        return -1;
+    int x = queue[front];
+    if (front == rear)
+        front = rear = -1;
+    else
+        front++;
+    return x;
+}
+
+// BFS function
+void bfs(int n, int adj[MAX][MAX], int s) {
+    int visited[MAX] = {0};
+
+    enqueue(s);
+    visited[s] = 1;
+
+    printf("BFS Traversal: ");
+
+    while (front != -1) {
+        int node = dequeue();
+        printf("%d ", node);
+
+        for (int i = 0; i < n; i++) {
+            if (adj[node][i] == 1 && !visited[i]) {
+                enqueue(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+// Main function
+int main() {
+    int n, s;
+    int adj[MAX][MAX];
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter source node: ");
+    scanf("%d", &s);
+
+    bfs(n, adj, s);
+
+    return 0;
+}
