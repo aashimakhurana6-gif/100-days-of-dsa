b5016a31deb36b40e1176e07599af9e2546cb942
Aashima Khurana <aashimakhurana6@gmail.com>
2026-04-18 22:32:10 +0530

    Add files via upload

---
+++ day77.c
@@ -0,0 +1,56 @@
+//Using BFS or DFS, check if the entire graph is connected.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];   // Adjacency matrix
+int visited[MAX];
+int n, m;
+
+// DFS function
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    int u, v;
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            adj[i][j] = 0;
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;   // Undirected graph
+    }
+
+    // Initialize visited array
+    for (int i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    // Start DFS from node 1
+    dfs(1);
+
+    // Check if all nodes are visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+    return 0;
+}
